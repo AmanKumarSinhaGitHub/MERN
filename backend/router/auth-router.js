9c8b6be44d3cfce168a60a4eb6028cff69bb8ab7
Aman Kumar Sinha <amankrsinha07@gmail.com>
2024-08-21 21:51:10 +0530

    Update

--- backend/router/auth-router.js
+++ backend/router/auth-router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { home, register, login } = require('../controllers/auth-controller');
-const { SignUpSchema } = require('../validators/auth-validator');
+const { SignUpSchema, LoginSchema } = require('../validators/auth-validator');
 const validate = require('../middlewares/validate-middleware');
 
 router.route('/')
@@ -12,7 +12,7 @@ router.route('/register')
     .post(validate(SignUpSchema), register); // Middleware to validate the request body
 
 router.route('/login')
-    .post(login);
+    .post(validate(LoginSchema), login);
 
 
 module.exports = router;
--- backend/validators/auth-validator.js
+++ backend/validators/auth-validator.js
@@ -27,4 +27,22 @@ const SignUpSchema = z.object({
         .max(255, { message: "Password must be at most 255 characters long" }),
 });
 
-module.exports = { SignUpSchema };
+
+
+const LoginSchema = z.object({
+    
+    email: z
+        .string({ required_error: "Email is required" })
+        .trim()
+        .min(3, { message: "Email must be at least 3 characters long" })
+        .max(255, { message: "Email must be at most 255 characters long" })
+        .email({ message: "Invalid email address", tldWhitelist: ["com", "net"] }),
+
+    
+    password: z
+        .string({ required_error: "Password is required" })
+        .min(6, { message: "Password must be at least 6 characters long" })
+        .max(255, { message: "Password must be at most 255 characters long" }),
+});
+
+module.exports = { SignUpSchema, LoginSchema };
